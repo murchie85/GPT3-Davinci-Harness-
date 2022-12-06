ec3836023dff238abb74c2f892623b46992cbe08
murchie85 <murchie85@gmail.com>
2022-12-06 18:49:32 +0000

    making the page look better

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ openai.api_key = keyFile.read()
 
 history = []
 
+
+#  POST CONVERSATION
 @app.route('/conversation', methods=['POST'])
 def conversation():
     question = request.form['question']
     history.append({'prompt': question})
     history_str = "\n".join([h['prompt'] for h in history])
-    response = openai.Completion.create(model="text-davinci-003", prompt=history_str, temperature=0, max_tokens=4000)
+
+    response = openai.Completion.create(model="text-davinci-003", prompt=history_str, temperature=0, max_tokens=3000)
 
     with open('conversation.txt', 'a') as f:
         f.write(f"{question}\n")
@@ -26,11 +29,23 @@ def conversation():
 
     return response
 
-
+#  CHAT
 @app.route('/chat')
 def chat():
     return render_template('chat.html')
 
+
+#  SHUTDOWN
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 if __name__ == '__main__':
     app.run()
---
+++ runme.sh
@@ -0,0 +1,2 @@
+cd /Users/adammcmurchie/code/tools/davinci
+/opt/anaconda3/bin/python main.py
